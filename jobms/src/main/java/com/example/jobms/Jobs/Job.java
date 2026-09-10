ffc026baa8ad76b82b21a5e98f08973de636071d
Jawahar <jawahar052005@gmail.com>
2026-09-10 12:50:32 +0530

    Add Dockerfiles for all 3 microservices

---
+++ jobms/src/main/java/com/example/jobms/Jobs/Job.java
@@ -0,0 +1,92 @@
+package com.example.jobms.Jobs;
+
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String minsalary;
+    private String maxsalary;
+    private String location;
+    private long companyid;
+
+
+
+    public Job() {
+    }
+
+    public Job(long id, String title, String description, String minsalary, String maxsalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minsalary = minsalary;
+        this.maxsalary = maxsalary;
+        this.location = location;
+
+    }
+
+    public long getCompanyid() {
+        return companyid;
+    }
+
+    public void setCompanyid(long companyid) {
+        this.companyid = companyid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMinsalary() {
+        return minsalary;
+    }
+
+    public void setMinsalary(String minsalary) {
+        this.minsalary = minsalary;
+    }
+
+    public String getMaxsalary() {
+        return maxsalary;
+    }
+
+    public void setMaxsalary(String maxsalary) {
+        this.maxsalary = maxsalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
+
+
---
+++ jobms/src/main/java/com/example/jobms/Jobs/jobcontroller.java
@@ -0,0 +1,55 @@
+package com.example.jobms.Jobs;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class jobcontroller {
+    Jobservice jobservice;
+
+    public jobcontroller(Jobservice jobservice) {
+        this.jobservice = jobservice;
+    }
+
+    @GetMapping("/job")
+    public ResponseEntity<List<Job>> findall(){
+        return ResponseEntity.ok(jobservice.find());
+    }
+
+    @PostMapping("/job")
+    public ResponseEntity<String> createall(@RequestBody Job job){
+        jobservice.create(job);
+        return new ResponseEntity<>("jobadded success", HttpStatus.OK);
+    }
+
+    @GetMapping("/job/{id}")
+    public ResponseEntity<Job> getjobbyid(@PathVariable Long id){
+        Job job=jobservice.getjobbyid(id);
+        if(job!=null)
+            return new ResponseEntity<>(job,HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
+
+
+    @DeleteMapping("/job/{id}")
+    public ResponseEntity<String> deletejobbyid(@PathVariable long id){
+        boolean delete=jobservice.deletejob(id);
+        if(delete)
+            return new ResponseEntity<>("job deleted successfully",HttpStatus.OK);
+        return new ResponseEntity<>("error",HttpStatus.NOT_FOUND);
+    }
+    @PutMapping("/job/{id}")
+    public ResponseEntity<String> update(@PathVariable long id,@RequestBody Job updatedjob){
+        boolean update=jobservice.updatejobbyid(id,updatedjob);
+        if(update)
+            return new ResponseEntity<>("job updated successfully",HttpStatus.OK);
+        return new ResponseEntity<>("job not updated successfully",HttpStatus.NOT_FOUND);
+    }
+
+}
